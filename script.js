3d557ec2b852c5f80565577b9af208bc7106d52b
jefferyrosario <jeffrosario92@gmail.com>
2024-10-06 17:13:35 -0400

    Update final html, css, javascript

--- script.js
+++ script.js
@@ -5,29 +5,6 @@ function createDiv() {
     return div
 }
 
-function createGrid(gridSize = 16, width = "20px", height = "20px") {
-
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
-            const div = createDiv()
-            div.classList.add("div")
-            div.style.height = height
-            div.style.width = width
-            container.appendChild(div)
-            if (j == gridSize - 1) {
-                const breakIt = document.createElement("secondBreak")
-                breakIt.classList.add("secondBreak")
-                container.appendChild(breakIt)
-            }
-        }
-    }
-    hover()
-}
-
-const container = document.querySelector(".container")
-
-createGrid(16)
-
 function hover() {
     const allDivs = document.querySelectorAll(".div")
     allDivs.forEach((divs) => {
@@ -38,9 +15,34 @@ function hover() {
     })
 }
 
+function createGrid(gridSize = 16) {
+    
+    let squareSize = 900 / gridSize
+
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const div = createDiv()
+            div.style.width = `${squareSize}px`
+            div.style.height = `${squareSize}px`
+            container.appendChild(div)
+            if (j == gridSize - 1) {
+                const breakIt = document.createElement("secondBreak")
+                breakIt.classList.add("secondBreak")
+                container.appendChild(breakIt)
+            }
+        }
+    }
+   hover()
+}
+
+
+
+
+const container = document.querySelector(".container")
 const buttons = document.querySelector(".button")
 let gridSize = 16
-console.log(container.getComputedStyle())
+createGrid(40)
+
 buttons.addEventListener('click', () => {
     if (container.firstChild) {
         while (container.firstChild) {
@@ -49,9 +51,7 @@ buttons.addEventListener('click', () => {
     }
     gridSizePromt = prompt("How big do you want your grid? ex. 5 will give a 5x5 grid.")
     gridSize = parseInt(gridSizePromt)
-    width = 20 + "px"
-    height = 20 + "px"
-    createGrid(gridSize, width = width, height = height)
+    createGrid(gridSize)
 })
 
 
